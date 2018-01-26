e0656b01350e23b280be76b4667101b71296cb51
Lucas OMS <lucas-loic.oms@etu.unice.fr>
2018-01-26 11:19:17 +0100

    ISE-23 Debug de fonction et ajout de l'analyse des biomes

--- src/main/java/fr/unice/polytech/si3/qgl/ise/parsing/Scan.java
+++ src/main/java/fr/unice/polytech/si3/qgl/ise/parsing/Scan.java
@@ -13,6 +13,9 @@ public class Scan {
     private ArrayList<Biome> biomes;
 
     public Scan(String scanResult) {
+        creeks = new ArrayList<>();
+        emergencySites = new ArrayList<>();
+        biomes = new ArrayList<>();
         JSONObject data = new JSONObject(scanResult);
         cost = data.getInt("cost");
         JSONObject extras = data.getJSONObject("extras");
@@ -26,11 +29,75 @@ public class Scan {
         }
         JSONArray biomesJson = extras.getJSONArray("biomes");
         for (int i = 0; i < biomesJson.length(); i++) {
-            if (sitesJson.getString(i).equals("OCEAN")) {
+            addBiome(biomesJson.getString(i));
+        }
+    }
+
+    private void addBiome(String biome) {
+        switch (biome) {
+            //region Common biomes
+            case "OCEAN":
                 biomes.add(Biome.OCEAN);
-            } else {
+                break;
+            case "LAKE":
+                biomes.add(Biome.LAKE);
+                break;
+            case "BEACH":
                 biomes.add(Biome.BEACH);
-            }
+                break;
+            case "GRASSLAND":
+                biomes.add(Biome.GRASSLAND);
+                break;
+            //endregion
+            //region Tropical biomes
+            case "MANGROVE":
+                biomes.add(Biome.MANGROVE);
+                break;
+            case "TROPICAL_RAIN_FOREST":
+                biomes.add(Biome.TROPICAL_RAIN_FOREST);
+                break;
+            case "TROPICAL_SEASONAL_FOREST":
+                biomes.add(Biome.TROPICAL_SEASONAL_FOREST);
+                break;
+            //endregion
+            //region Temperate biomes
+            case "TEMPERATE_DECIDUOUS_FOREST":
+                biomes.add(Biome.TEMPERATE_DECIDUOUS_FOREST);
+                break;
+            case "TEMPERATE_RAIN_FOREST":
+                biomes.add(Biome.TEMPERATE_RAIN_FOREST);
+                break;
+            case "TEMPERATE_DESERT":
+                biomes.add(Biome.TEMPERATE_DESERT);
+                break;
+            //endregion
+            //region Nordic/montain biomes
+            case "TAIGA":
+                biomes.add(Biome.TAIGA);
+                break;
+            case "SNOW":
+                biomes.add(Biome.SNOW);
+                break;
+            case "TUNDRA":
+                biomes.add(Biome.TUNDRA);
+                break;
+            case "ALPINE":
+                biomes.add(Biome.ALPINE);
+                break;
+            case "GLACIER":
+                biomes.add(Biome.GLACIER);
+                break;
+            //endregion
+            //region Subtropical biomes
+            case "SHRUBLAND":
+                biomes.add(Biome.SHRUBLAND);
+                break;
+            case "SUB_TROPICAL_DESERT":
+                biomes.add(Biome.SUB_TROPICAL_DESERT);
+                break;
+            //endregion
+            default:
+                throw new IllegalArgumentException("This biome doesn't exist");
         }
     }
 
