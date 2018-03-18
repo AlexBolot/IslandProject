f136e9a9f41d043c61c045bc371126179eb5d96d
Quentin Guimard <quentin.guimard@etu.unice.fr>
2018-03-18 16:00:56 +0100

    ISE-47

--- src/main/java/fr/unice/polytech/si3/qgl/ise/map/PathFinder.java
+++ src/main/java/fr/unice/polytech/si3/qgl/ise/map/PathFinder.java
@@ -6,101 +6,67 @@ import fr.unice.polytech.si3.qgl.ise.enums.RawResource;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class PathFinder
-{
+public class PathFinder {
 
     /**
-     Calculates the distance between two points on the map
-
-     @param c1 : the coordinates of one point on the map
-     @param c2 : the coordinates of another point on the map
-     @return the distance between these two points
+     * Calculates the distance between two points on the map
+     *
+     * @param c1 : the coordinates of one point on the map
+     * @param c2 : the coordinates of another point on the map
+     * @return the distance between these two points
      */
-    public static double calculateDistance (Coordinates c1, Coordinates c2)
-    {
+    public static double calculateDistance(Coordinates c1, Coordinates c2) {
         if (c1.equals(c2)) return 0;
         return Math.sqrt(Math.pow((double) c1.getX() - c2.getX(), 2) + Math.pow((double) c1.getY() - c2.getY(), 2));
     }
 
     /**
-     Searches for the creek nearest to the given point on the island from what's recorded by the drone
-
-     @param creeks      : map containing the recorded creeks (ids and coordinates)
-     @param coordinates : one point on the map
-     @return the id of the creek nearest to the given point, or an empty string if there is no creek
+     * Searches for the creek nearest to the given point on the island from what's recorded by the drone
+     *
+     * @param creeks      : map containing the recorded creeks (ids and coordinates)
+     * @param coordinates : one point on the map
+     * @return the id of the creek nearest to the given point, or an empty string if there is no creek
      */
-    public static String findNearestCreek (Map<String, Coordinates> creeks, Coordinates coordinates)
-    {
-        return creeks.entrySet()
-                     .stream()
-                     .min(Comparator.comparingDouble(entry -> calculateDistance(entry.getValue(), coordinates)))
-                     .map(Map.Entry::getKey)
-                     .orElse("");
+    public static String findNearestCreek(Map<String, Coordinates> creeks, Coordinates coordinates) {
+        return creeks.entrySet().stream()
+                .min(Comparator.comparingDouble(entry -> calculateDistance(entry.getValue(), coordinates)))
+                .map(Map.Entry::getKey)
+                .orElse("");
     }
 
     /**
-     Searches for the nearest tile belonging to the given biome from the given coordinates
-
-     @param map         : the map containing the tiles
-     @param coordinates : the coordinates from where the search is led
-     @param biome       : the biome containing tiles to search for
-     @return the coordinates of the nearest tiles or null if there is no such tile
+     * Searches for the nearest tile belonging to the given biome from the given coordinates
+     *
+     * @param map         : the map containing the tiles
+     * @param coordinates : the coordinates from where the search is led
+     * @param biome       : the biome containing tiles to search for
+     * @return the coordinates of the nearest tiles or null if there is no such tile
      */
-    public static Coordinates findNearestTileOfBiome (IslandMap map, Coordinates coordinates, Biome biome)
-    {
-        return map.getMap()
-                  .entrySet()
-                  .stream()
-                  .filter(entry -> entry.getValue().getPossibleBiomes().contains(biome))
-                  .min(Comparator.comparingDouble(entry -> calculateDistance(entry.getKey(), coordinates)))
-                  .map(Map.Entry::getKey)
-                  .orElse(null);
+    public static Coordinates findNearestTileOfBiome(IslandMap map, Coordinates coordinates, Biome biome) {
+        return map.getMap().entrySet().stream()
+                .filter(entry -> entry.getValue().getPossibleBiomes().contains(biome))
+                .min(Comparator.comparingDouble(entry -> calculateDistance(entry.getKey(), coordinates)))
+                .map(Map.Entry::getKey)
+                .orElse(null);
     }
 
     /**
-     Searches for the creek nearest to given coordinates
-
-     @param creeks      : map containing the recorded creeks (ids and coordinates)
-     @param coordinates : coordinates of the point to base onto
-     @return the id of the nearst creek to the point of given coordinates
+     * Finds the nearest tile that possibly contains the given raw resource
+     *
+     * @param map         : the map containing the tiles
+     * @param coordinates : the coordinates from where the search is led
+     * @param resource    : resource to look for
+     * @return the coordinates of the nearest tile that possibly contains the given raw resource, or null if there is no such tile
      */
-    public static String findNearesCreek (Map<String, Coordinates> creeks, Coordinates coordinates)
-    {
-        return creeks.entrySet()
-                     .stream()
-                     .sorted(Comparator.comparingDouble(creekCoords -> calculateDistance(creekCoords.getValue(),
-                                                                                         coordinates)))
-                     .map(Map.Entry::getKey)
-                     .collect(Collectors.toList())
-                     .get(0);
+    public static Coordinates findNearestTileOfResource(IslandMap map, Coordinates coordinates, RawResource resource) {
+        List<Biome> acceptableBiomes = Arrays.stream(Biome.values())
+                .filter(biome -> Arrays.asList(biome.getResources()).contains(resource))
+                .collect(Collectors.toList());
+
+        return acceptableBiomes.stream()
+                .map(biome -> findNearestTileOfBiome(map, coordinates, biome))
+                .filter(Objects::nonNull)
+                .min(Comparator.comparingDouble(tileCoordinates -> calculateDistance(tileCoordinates, coordinates)))
+                .orElse(null);
     }
-
-    /**
-     Finds the first tile that possibly contains the given rawResource
-
-     @param rawResource : Resource to look for
-     @param map         : Scanned Map that contains the biomes found
-     @return the coordinates of the Tile that possibly contains the given rawResource
-     */
-    public static Optional<Coordinates> findRessourceTile (RawResource rawResource, IslandMap map)
-    {
-        ArrayList<Biome> acceptableBiomes = Arrays.stream(Biome.values())
-                                                  .filter(biome -> Arrays.asList(biome.getResources())
-                                                                         .contains(rawResource))
-                                                  .collect(Collectors.toCollection(ArrayList::new));
-
-        for (Map.Entry<Coordinates, Tile> entry : map.getMap().entrySet())
-        {
-            for (Biome acceptableBiome : acceptableBiomes)
-            {
-                for (Biome biome : entry.getValue().getPossibleBiomes())
-                {
-                    if (biome == acceptableBiome) return Optional.of(entry.getKey());
-                }
-            }
-        }
-
-        return Optional.empty();
-    }
-    //TODO findNearestTileOfResource
 }
--- src/test/java/fr/unice/polytech/si3/qgl/ise/map/PathFinderTest.java
+++ src/test/java/fr/unice/polytech/si3/qgl/ise/map/PathFinderTest.java
@@ -1,46 +1,22 @@
 package fr.unice.polytech.si3.qgl.ise.map;
 
 import fr.unice.polytech.si3.qgl.ise.enums.Biome;
+import fr.unice.polytech.si3.qgl.ise.enums.RawResource;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
 public class PathFinderTest {
 
-    @Test
-    public void calculateDistanceTest() {
-        assertEquals(0.0, PathFinder.calculateDistance(new Coordinates(0, 0), new Coordinates(0, 0)), 0);
-        assertEquals(5.0, PathFinder.calculateDistance(new Coordinates(0, 3), new Coordinates(4, 0)), 0);
-        assertEquals(5.0, PathFinder.calculateDistance(new Coordinates(0, -3), new Coordinates(-4, 0)), 0);
-        assertEquals(1.41421356, PathFinder.calculateDistance(new Coordinates(0, 1), new Coordinates(1, 0)), 0.00000001);
-        assertEquals(93.14504817756, PathFinder.calculateDistance(new Coordinates(-30, -13), new Coordinates(-6, 77)), 0.00000000001);
-    }
+    private IslandMap map;
 
-    @Test
-    public void findNearestCreekTest() {
-        IslandMap map = new IslandMap();
-
-        map.addCreek(new Coordinates(945, 990), "id0");
-        map.addCreek(new Coordinates(648, -976), "id1");
-        map.addCreek(new Coordinates(-670, 300), "id2");
-        map.addCreek(new Coordinates(376, 833), "id3");
-        Coordinates coordinates = new Coordinates(-146, -975);
-
-        assertEquals("id1", PathFinder.findNearestCreek(map.getCreeks(), coordinates));
-
-        map.addCreek(new Coordinates(-146, -975), "id4");
-
-        assertEquals("id4", PathFinder.findNearestCreek(map.getCreeks(), coordinates));
-    }
-
-    @Test
-    public void findNearestTileOfBiomeTest() {
-        IslandMap map = new IslandMap();
+    @Before
+    public void init() {
+        map = new IslandMap();
 
         Map<Biome, Double> biomesPercentage1 = new HashMap<>();
         biomesPercentage1.put(Biome.ALPINE, 24.8);
@@ -59,13 +35,51 @@ public class PathFinderTest {
         Tile tile3 = new Tile(biomesPercentage3);
 
         map.addTile(new Coordinates(23, -40), tile1);
-        map.addTile(new Coordinates(13, 46), tile2);
+        map.addTile(new Coordinates(13, 39), tile2);
         map.addTile(new Coordinates(15, -34), tile3);
 
+        map.addCreek(new Coordinates(945, 990), "id0");
+        map.addCreek(new Coordinates(648, -976), "id1");
+        map.addCreek(new Coordinates(-670, 300), "id2");
+        map.addCreek(new Coordinates(376, 833), "id3");
+    }
+
+    @Test
+    public void calculateDistanceTest() {
+        assertEquals(0.0, PathFinder.calculateDistance(new Coordinates(0, 0), new Coordinates(0, 0)), 0);
+        assertEquals(5.0, PathFinder.calculateDistance(new Coordinates(0, 3), new Coordinates(4, 0)), 0);
+        assertEquals(5.0, PathFinder.calculateDistance(new Coordinates(0, -3), new Coordinates(-4, 0)), 0);
+        assertEquals(1.41421356, PathFinder.calculateDistance(new Coordinates(0, 1), new Coordinates(1, 0)), 0.00000001);
+        assertEquals(93.14504817756, PathFinder.calculateDistance(new Coordinates(-30, -13), new Coordinates(-6, 77)), 0.00000000001);
+    }
+
+    @Test
+    public void findNearestCreekTest() {
+        Coordinates coordinates = new Coordinates(-146, -975);
+
+        assertEquals("id1", PathFinder.findNearestCreek(map.getCreeks(), coordinates));
+
+        map.addCreek(new Coordinates(-146, -975), "id4");
+
+        assertEquals("id4", PathFinder.findNearestCreek(map.getCreeks(), coordinates));
+    }
+
+    @Test
+    public void findNearestTileOfBiomeTest() {
         assertEquals(null, PathFinder.findNearestTileOfBiome(map, new Coordinates(0, 0), Biome.SHRUBLAND));
         assertEquals(new Coordinates(15, -34), PathFinder.findNearestTileOfBiome(map, new Coordinates(-20, -26), Biome.TAIGA));
         assertEquals(new Coordinates(15, -34), PathFinder.findNearestTileOfBiome(map, new Coordinates(0, 0), Biome.TUNDRA));
-        assertEquals(new Coordinates(23, -40), PathFinder.findNearestTileOfBiome(map, new Coordinates(0, 0), Biome.ALPINE));
+        assertEquals(new Coordinates(13, 39), PathFinder.findNearestTileOfBiome(map, new Coordinates(0, 0), Biome.ALPINE));
         assertEquals(new Coordinates(23, -40), PathFinder.findNearestTileOfBiome(map, new Coordinates(39, -45), Biome.GLACIER));
+    }
+
+    @Test
+    public void findNearestTileOfResourceTest() {
+        assertEquals(null, PathFinder.findNearestTileOfResource(map, new Coordinates(24,-42), RawResource.FRUITS));
+        assertEquals(null, PathFinder.findNearestTileOfResource(map, new Coordinates(-17,2), RawResource.FISH));
+        assertEquals(new Coordinates(15, -34), PathFinder.findNearestTileOfResource(map, new Coordinates(6,-14), RawResource.WOOD));
+        assertEquals(new Coordinates(15, -34), PathFinder.findNearestTileOfResource(map, new Coordinates(0,0), RawResource.FUR));
+        assertEquals(new Coordinates(13, 39), PathFinder.findNearestTileOfResource(map, new Coordinates(0,0), RawResource.FLOWER));
+        assertEquals(new Coordinates(13, 39), PathFinder.findNearestTileOfResource(map, new Coordinates(0,0), RawResource.ORE));
     }
 }
