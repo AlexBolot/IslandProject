3af4b789e53a3c1037df1779a608b855c655211c
Lucas OMS <lucas-loic.oms@etu.unice.fr>
2018-03-22 11:06:47 +0100

    Fix tests

--- src/main/java/fr/unice/polytech/si3/qgl/ise/entities/Crew.java
+++ src/main/java/fr/unice/polytech/si3/qgl/ise/entities/Crew.java
@@ -17,38 +17,36 @@ import fr.unice.polytech.si3.qgl.ise.map.PathFinder;
 
 import java.util.*;
 
-public class Crew
-{
+public class Crew {
     private static final int movementUnit = 1;
 
     private JsonFactory json;
 
     private Action lastAction;
 
-    private boolean                         isLanded;
-    private String                          idCreek;
-    private List<RawContract>               rawContracts;
-    private List<CraftedContract>           craftedContracts;
-    private Integer                         crewSize;
+    private boolean isLanded;
+    private String idCreek;
+    private List<RawContract> rawContracts;
+    private List<CraftedContract> craftedContracts;
+    private Integer crewSize;
     private HashMap<RawResource, Abundance> lastExplore;
-    private ArrayList<Action>               steps;
+    private ArrayList<Action> steps;
 
-    private IslandMap   map;
+    private IslandMap map;
     private Coordinates coords;
     private RawResource currentResource;
 
     /**
-     While the coord is an hypothesis and we didn't have a situation that can makes us sure of where we are
+     * While the coord is an hypothesis and we didn't have a situation that can makes us sure of where we are
      */
     private boolean knowExactPosition;
 
     /**
-     Objective where we want the crew to go.
+     * Objective where we want the crew to go.
      */
     private Coordinates objective;
 
-    public Crew (IslandMap map, int crewSize, List<RawContract> rawContracts, List<CraftedContract> craftedContracts)
-    {
+    public Crew(IslandMap map, int crewSize, List<RawContract> rawContracts, List<CraftedContract> craftedContracts) {
         this.map = map;
         this.rawContracts = rawContracts;
         this.craftedContracts = craftedContracts;
@@ -65,8 +63,7 @@ public class Crew
         initActions();
     }
 
-    private void initActions ()
-    {
+    private void initActions() {
         steps = new ArrayList<>();
         steps.add(new Land(this, idCreek, crewSize));
         steps.add(new Move_to(this, objective));
@@ -74,12 +71,10 @@ public class Crew
         steps.add(new StopAction());
     }
 
-    public String takeDecision ()
-    {
+    public String takeDecision() {
         String res;
 
-        for (Action step : steps)
-        {
+        for (Action step : steps) {
             lastAction = step;
 
             if (lastAction.isFinished()) continue;
@@ -94,62 +89,51 @@ public class Crew
         return new StopAction().apply();
     }
 
-    public void acknowledgeResults (String results)
-    {
-        if (lastAction instanceof CrewAction)
-        {
+    public void acknowledgeResults(String results) {
+        if (lastAction instanceof CrewAction) {
             ((CrewAction) lastAction).acknowledgeResults(this, results);
         }
         //else would be only for stop action <=> do nothing
     }
 
-    public IslandMap getMap ()
-    {
+    public IslandMap getMap() {
         return map;
     }
 
-    public boolean isLanded ()
-    {
+    public boolean isLanded() {
         return isLanded;
     }
 
-    public Coordinates getCoords ()
-    {
+    public Coordinates getCoords() {
         return coords;
     }
 
-    public void setCoords (Coordinates coords)
-    {
+    public void setCoords(Coordinates coords) {
         this.coords = coords;
     }
 
-    public void setLastExplore (HashMap<RawResource, Abundance> resources)
-    {
+    public void setLastExplore(HashMap<RawResource, Abundance> resources) {
         this.lastExplore = resources;
     }
 
-    public HashMap<RawResource, Abundance> getLastExplore ()
-    {
+    public HashMap<RawResource, Abundance> getLastExplore() {
         return lastExplore;
     }
 
-    public void setCurrentResource (RawResource currentResource)
-    {
+    public void setCurrentResource(RawResource currentResource) {
         this.currentResource = currentResource;
     }
 
-    public RawResource getCurrentResource ()
-    {
+    public RawResource getCurrentResource() {
         return currentResource;
     }
 
-    public void setIdCreek (String id)
-    {
+    public void setIdCreek(String id) {
+        this.coords = map.getCreeks().get(id);
         this.idCreek = id;
     }
 
-    public void setCrewSize (int size)
-    {
+    public void setCrewSize(int size) {
         this.crewSize = size;
     }
 }
--- src/test/java/fr/unice/polytech/si3/qgl/ise/actions/crew/Move_toTest.java
+++ src/test/java/fr/unice/polytech/si3/qgl/ise/actions/crew/Move_toTest.java
@@ -1,26 +1,38 @@
 package fr.unice.polytech.si3.qgl.ise.actions.crew;
 
+import fr.unice.polytech.si3.qgl.ise.CraftedContract;
+import fr.unice.polytech.si3.qgl.ise.RawContract;
 import fr.unice.polytech.si3.qgl.ise.actions.CrewAction;
 import fr.unice.polytech.si3.qgl.ise.entities.Crew;
+import fr.unice.polytech.si3.qgl.ise.enums.RawResource;
 import fr.unice.polytech.si3.qgl.ise.map.Coordinates;
 import fr.unice.polytech.si3.qgl.ise.map.IslandMap;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
-public class Move_ToTest {
+public class Move_toTest {
 
     Crew crew;
     CrewAction moveTo;
     IslandMap map;
+    List<RawContract> rawContracts;
+    List<CraftedContract> craftedContracts;
 
     @Before
     public void setup() {
         map = new IslandMap();
         map.addCreek(new Coordinates(0, 0), "idDepart");
 
-        crew = new Crew(map, 10, null, null);
+        rawContracts = new ArrayList<>();
+        craftedContracts = new ArrayList<>();
+
+        rawContracts.add(new RawContract(RawResource.WOOD, 500));
+        crew = new Crew(map, 10, rawContracts, craftedContracts);
         crew.setIdCreek("idDepart");
     }
 
