2f16f868418bbc564e2ace3d1d8137ecd7d438bf
Lucas OMS <lucas-loic.oms@etu.unice.fr>
2018-03-31 18:59:48 +0200

    ISE-65 Création d'une nouvelle fonction de parse pour les map

--- src/main/java/fr/unice/polytech/si3/qgl/ise/actions/crew/Transform.java
+++ src/main/java/fr/unice/polytech/si3/qgl/ise/actions/crew/Transform.java
@@ -2,12 +2,15 @@ package fr.unice.polytech.si3.qgl.ise.actions.crew;
 
 import fr.unice.polytech.si3.qgl.ise.actions.CrewAction;
 import fr.unice.polytech.si3.qgl.ise.entities.Crew;
+import fr.unice.polytech.si3.qgl.ise.enums.CraftedResource;
 import fr.unice.polytech.si3.qgl.ise.enums.RawResource;
+import fr.unice.polytech.si3.qgl.ise.factories.JsonFactory;
 
 import java.util.HashMap;
 
 public class Transform extends CrewAction {
     private HashMap<RawResource, Integer> resourceWithQuantity;
+    private HashMap<CraftedResource, Integer> crafted;
 
     public Transform(Crew crewToUpdate, HashMap<RawResource, Integer> resourceWithQuantity) {
         super(crewToUpdate);
@@ -16,7 +19,8 @@ public class Transform extends CrewAction {
 
     @Override
     public String apply() {
-        return null;
+        ;
+        return new JsonFactory().createJsonString("transform", resourceWithQuantity);
     }
 
     @Override
